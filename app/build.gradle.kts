954b09c6bf384e222310d0860531a291870ec090
rnethula2689 <122056429+rnethula2689@users.noreply.github.com>
2026-06-22 14:27:22 -0500

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.stalkertv.app"
         minSdk = 21
         targetSdk = 34
-        versionCode = 9
-        versionName = "0.9"
+        versionCode = 10
+        versionName = "0.10"
     }
 
     signingConfigs {
